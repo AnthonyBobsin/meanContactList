edcb57a162435f8540d7d14135907aee9193860e
AnthonyBobsin <bobsinj@gmail.com>
2015-05-27 21:26:19 -0400

    Sets up dummy contacts, and uses ng-repeat to load them into the table

--- public/javascripts/controllers/controller.js
+++ public/javascripts/controllers/controller.js
@@ -2,4 +2,26 @@ var myApp = angular.module('MyApp', [])
 
 myApp.controller('AppController', ['$scope', '$http', function($scope, $http) {
 	console.log("Hello World from client side controller")
+
+	var personOne = {
+		name: 'Alex',
+		email: 'alex@email.com',
+		number: '444-555-6666'
+	}
+
+	var personTwo = {
+		name: 'Emily',
+		email: 'emily@email.com',
+		number: '444-666-5555'
+	}
+
+	var personThree = {
+		name: 'John',
+		email: 'johnAppleseed@email.com',
+		number: '333-444-5555'
+	}
+
+	var contactList = [personOne, personTwo, personThree]
+	$scope.contactList = contactList
+
 }])

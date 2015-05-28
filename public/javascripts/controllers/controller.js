ec7650006988292246a48734a5afd7d98e07c0c2
AnthonyBobsin <bobsinj@gmail.com>
2015-05-27 21:32:13 -0400

    Moves dummy contacts to server, and makes a get request for data from client side controller

--- public/javascripts/controllers/controller.js
+++ public/javascripts/controllers/controller.js
@@ -3,25 +3,8 @@ var myApp = angular.module('MyApp', [])
 myApp.controller('AppController', ['$scope', '$http', function($scope, $http) {
 	console.log("Hello World from client side controller")
 
-	var personOne = {
-		name: 'Alex',
-		email: 'alex@email.com',
-		number: '444-555-6666'
-	}
-
-	var personTwo = {
-		name: 'Emily',
-		email: 'emily@email.com',
-		number: '444-666-5555'
-	}
-
-	var personThree = {
-		name: 'John',
-		email: 'johnAppleseed@email.com',
-		number: '333-444-5555'
-	}
-
-	var contactList = [personOne, personTwo, personThree]
-	$scope.contactList = contactList
-
+	$http.get('/contactlist').success(function(response) {
+		console.log("I got the data I requested")
+		$scope.contactList = response
+	})
 }])
--- server.js
+++ server.js
@@ -3,5 +3,30 @@ var express = require('express'),
 
 app.use(express.static(__dirname + "/public"))
 
+app.get('/contactlist', function(req, res) {
+	console.log("I received a GET request")
+
+	var personOne = {
+		name: 'Alex',
+		email: 'alex@email.com',
+		number: '444-555-6666'
+	}
+
+	var personTwo = {
+		name: 'Emily',
+		email: 'emily@email.com',
+		number: '444-666-5555'
+	}
+
+	var personThree = {
+		name: 'John',
+		email: 'johnAppleseed@email.com',
+		number: '333-444-5555'
+	}
+
+	var contactList = [personOne, personTwo, personThree]
+	res.json(contactList)
+})
+
 app.listen(3000)
 console.log("Server running on port 3000")

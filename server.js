61734244ab82fb3ad23d4a3426dcfafa78b50ee0
AnthonyBobsin <bobsinj@gmail.com>
2015-06-28 20:09:29 -0400

    Includes mongojs node module and requires it in server

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
 var express = require('express'),
-		app = express()
+		mongojs = require('mongojs'),
+		app = express(),
+		db = mongojs('contactlist', ['contactlist'])
 
 app.use(express.static(__dirname + "/public"))
 
 app.get('/contactlist', function(req, res) {
 	console.log("I received a GET request")
 
-	var personOne = {
-		name: 'Alex',
-		email: 'alex@email.com',
-		number: '444-555-6666'
-	}
+	// var personOne = {
+	// 	name: 'Alex',
+	// 	email: 'alex@email.com',
+	// 	number: '444-555-6666'
+	// }
 
-	var personTwo = {
-		name: 'Emily',
-		email: 'emily@email.com',
-		number: '444-666-5555'
-	}
+	// var personTwo = {
+	// 	name: 'Emily',
+	// 	email: 'emily@email.com',
+	// 	number: '444-666-5555'
+	// }
 
-	var personThree = {
-		name: 'John',
-		email: 'johnAppleseed@email.com',
-		number: '333-444-5555'
-	}
+	// var personThree = {
+	// 	name: 'John',
+	// 	email: 'johnAppleseed@email.com',
+	// 	number: '333-444-5555'
+	// }
 
 	var contactList = [personOne, personTwo, personThree]
 	res.json(contactList)

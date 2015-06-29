1a08c959700766fd8b2bb9dc0a29d192e1b7dd47
AnthonyBobsin <bobsinj@gmail.com>
2015-06-28 20:30:05 -0400

    Updates contact list on success of contact creation

--- public/javascripts/controllers/controller.js
+++ public/javascripts/controllers/controller.js
@@ -3,16 +3,21 @@ var myApp = angular.module('MyApp', [])
 myApp.controller('AppController', ['$scope', '$http', function($scope, $http) {
 	console.log("Hello World from client side controller")
 
-	$http.get('/contactlist').success(function(response) {
-		console.log("I got the data I requested")
-		$scope.contactList = response
-	})
+  var refresh = function() {
+    $http.get('/contactlist').success(function(response) {
+      console.log("I got the data I requested")
+      $scope.contactList = response
+      $scope.newContact = ""
+    })
+  }
+
+  refresh()
 
   $scope.addContact = function() {
     console.log($scope.newContact)
     $http.post('/contactlist', $scope.newContact).success(function(response) {
       console.log(response)
-      $scope.contactList.add(response)
+      refresh()
     })
   }
 }])
